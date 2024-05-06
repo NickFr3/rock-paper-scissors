ea2209cfc20c2edb8e8b4abb7976d9be4d17c59e
NickFr3 <151786343+NickFr3@users.noreply.github.com>
2024-05-06 17:02:43 +0200

    Update app.js, working logic, points display missing

--- app.js
+++ app.js
@@ -40,77 +40,107 @@ function playGame() {
     let playerWins = 0;
     let computerWins = 0;
 
-    // Play until someone reaches 5 points
-    while (playerWins < 5 && computerWins < 5) {
+    // Create buttons to play game in ui
+    const rockButton = document.createElement("button");
+    rockButton.textContent = 'Rock';
+    const paperButton = document.createElement("button");
+    paperButton.textContent = 'Paper';
+    const scissorsButton = document.createElement("button");
+    scissorsButton.textContent = 'Scissors';
 
-        // Create buttons to play game in ui
-        const rockButton = document.createElement("button");
-        const paperButton = document.createElement("button");
-        const scissorsButton = document.createElement("button");
+    // Append buttons to a div to display them
+    const buttonsDiv = document.createElement("div");
 
-        // Append buttons to a div to display them
-        const buttonsDiv = document.createElement("div");
+    buttonsDiv.appendChild(rockButton);
+    buttonsDiv.appendChild(paperButton);
+    buttonsDiv.appendChild(scissorsButton);
 
-        buttonsDiv.appendChild(rockButton);
-        buttonsDiv.appendChild(paperButton);
-        buttonsDiv.appendChild(scissorsButton);
+    document.body.appendChild(buttonsDiv);
 
-        document.body.appendChild(buttonsDiv);
-
-        // Check for click and return result using playRound() function
-        rockButton.addEventListener("click", () => {
-            // Initialise computer selection
-            let computerSelection = getComputerChoice();
-            
-            let roundResult = playRound('rock', computerSelection);
-
-            if (roundResult.includes('win')) {
-                playerWins += 1;
-            } else if (roundResult.includes('lose')) {
-                computerWins += 1;
-            }
-        });
-        paperButton.addEventListener("click", () => {
-            // Initialise computer selection
-            let computerSelection = getComputerChoice();
-
-            playRound('paper', computerSelection);
-
-            if (roundResult.includes('win')) {
-                playerWins += 1;
-            } else if (roundResult.includes('lose')) {
-                computerWins += 1;
-            }
-        });
-        scissorsButton.addEventListener("click", () => {
-            // Initialise computer selection
-            let computerSelection = getComputerChoice();
-
-            playRound('scissors', computerSelection);
-
-            if (roundResult.includes('win')) {
-                playerWins += 1;
-            } else if (roundResult.includes('lose')) {
-                computerWins += 1;
-            }
-        });
-
+    // Check for click and return result using playRound() function
+    rockButton.addEventListener("click", () => {
+        // Initialise computer selection
+        let computerSelection = getComputerChoice();
         
-    }
-        
+        let roundResult = playRound('rock', computerSelection);
 
-    // Print game results
-    console.log(`Final results: Player [${playerWins} points] Computer [${computerWins} points]`);
+        if (roundResult.includes('win')) {
+            playerWins += 1;
+        } else if (roundResult.includes('lose')) {
+            computerWins += 1;
+        }
 
-    if (playerWins > computerWins) {
-        console.log('Player wins!');
-    } else if (playerWins < computerWins) {
-        console.log('Computer wins!');
-    } else {
-        console.log('It\'s a tie!')
-    }
+        if (playerWins == 5) {
+            alert('Player wins the game!');
 
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
 
+        } else if (computerWins == 5) {
+            alert('Computer wins the game!');
+
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+        }
+    });
+
+    paperButton.addEventListener("click", () => {
+        // Initialise computer selection
+        let computerSelection = getComputerChoice();
+
+        let roundResult = playRound('paper', computerSelection);
+
+        if (roundResult.includes('win')) {
+            playerWins += 1;
+        } else if (roundResult.includes('lose')) {
+            computerWins += 1;
+        }
+
+        if (playerWins == 5) {
+            alert('Player wins the game!');
+
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+
+        } else if (computerWins == 5) {
+            alert('Computer wins the game!');
+
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+        }
+    });
+
+    scissorsButton.addEventListener("click", () => {
+        // Initialise computer selection
+        let computerSelection = getComputerChoice();
+
+        let roundResult = playRound('scissors', computerSelection);
+
+        if (roundResult.includes('win')) {
+            playerWins += 1;
+        } else if (roundResult.includes('lose')) {
+            computerWins += 1;
+        }
+
+        if (playerWins == 5) {
+            alert('Player wins the game!');
+
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+
+        } else if (computerWins == 5) {
+            alert('Computer wins the game!');
+
+            rockButton.disabled = true;
+            paperButton.disabled = true;
+            scissorsButton.disabled = true;
+        }
+    });
 }
 
 playGame();
